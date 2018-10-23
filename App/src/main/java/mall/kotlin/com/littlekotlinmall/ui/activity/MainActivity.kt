753dbacaae2cf09319e20a333d112c939fe6a54c
maoai_xianyu <194264514@qq.com>
2018-10-23 17:31:10 +0800

    kotlin 电商项目实战

---
+++ App/src/main/java/mall/kotlin/com/littlekotlinmall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package mall.kotlin.com.littlekotlinmall.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import mall.kotlin.com.littlekotlinmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

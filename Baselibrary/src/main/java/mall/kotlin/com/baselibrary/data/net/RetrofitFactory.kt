de3ee0460627b56987c8e102eed79f6a54c4b1c4
DESKTOP-QF5VTOO\zhangkun <maoai_xianyu@outlook.com>
2018-10-26 08:25:38 +0800

    添加Retrofit

--- Baselibrary/src/main/java/mall/kotlin/com/baselibrary/data/net/RetrofitFactory.kt
+++ Baselibrary/src/main/java/mall/kotlin/com/baselibrary/data/net/RetrofitFactory.kt
@@ -3,4 +3,11 @@ package mall.kotlin.com.baselibrary.data.net
 /**
  * author:  zhangkun .
  * date:    on 2018/10/25.
+ * 单例
  */
+class RetrofitFactory private constructor(){
+    companion object {
+        
+    }
+
+}
